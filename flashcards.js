246c87d22ca469e985bc3e4584349874d52bdbb1
Adrian Shepelavey <adrianshepelavey@gmail.com>
2015-07-13 14:37:53 -0400

    event firing progress

--- flashcards.js
+++ flashcards.js
@@ -16,17 +16,23 @@ $(document).ready(function() {
     // ["Pennsylvania", "Harrisburg"]];
 
 //this one returns the values by index:
-var statesCapitals = [
-  [["Connecticut"], ["Hartford"]],
-  [["New Jersey"], ["Trenton"]],
-  [["New York"], ["Albany"]],
-  [["Ohio"], ["Columbus"]],
-  [["Pennsylvania"], ["Harrisburg"]],
-];
+  var statesCapitals = [
+    [["Connecticut"], ["Hartford"]],
+    [["New Jersey"], ["Trenton"]],
+    [["New York"], ["Albany"]],
+    [["Ohio"], ["Columbus"]],
+    [["Pennsylvania"], ["Harrisburg"]],
+  ];
+
+  $(".qBox").on("click", function(){
+      $(".qBox").html("What is the state capital of " + statesCapitals[0][0] + " ?")
+    })
+
+  // $(".div").on("keydown", function(evt){
+  //   if(evt.keyCode === 70){
+  //     console.log("f key firing")
+  //     }
+      //$(".qBox").html(statesCapitals[0][0])
+  //  })
 
 })
-
-$("#letter").on("keydown", function(evt){
-    if(evt.keyCode === 13){
-
-$(".qBox").html(statesCapitals[0][0])
